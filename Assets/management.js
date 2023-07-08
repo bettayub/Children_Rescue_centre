48a29f6752a78968098d1e2ac8b603444a81ed0f
bettayub <bettayub19@gmail.com>
2023-07-08 16:45:11 +0300

    deploying

--- Assets/management.js
+++ Assets/management.js
@@ -1,3 +1,46 @@
+// fetch('db.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const contributionsDiv = document.getElementById('contributions');
+//     contributionsDiv.innerHTML = ''; // Clear existing contributions
+
+//     if (data.contributions.length === 0) {
+//       contributionsDiv.innerHTML = 'No contributions found.';
+//     } else {
+//       data.contributions.forEach(contribution => {
+//         const contributionDiv = document.createElement('div');
+//         contributionDiv.classList.add('contribution');
+
+//         const header = document.createElement('div');
+//         header.classList.add('contribution-header');
+//         header.textContent = contribution.names.join(', ');
+//         contributionDiv.appendChild(header);
+
+//         const details = document.createElement('div');
+//         details.classList.add('contribution-details');
+//         details.innerHTML = `
+//           <div><span>Amount:</span> $${contribution.amount}</div>
+//           <div><span>Date:</span> ${contribution.date}</div>
+//           <div><span>Payment Method:</span> ${contribution.paymentMethod}</div>
+//           <div><span>Email:</span> ${contribution.email}</div>
+//         `;
+//         contributionDiv.appendChild(details);
+
+//         const message = document.createElement('div');
+//         message.classList.add('contribution-message');
+//         message.textContent = contribution.message;
+//         contributionDiv.appendChild(message);
+
+//         contributionsDiv.appendChild(contributionDiv);
+//       });
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Error fetching db.json:', error);
+//     const contributionsDiv = document.getElementById('contributions');
+//     contributionsDiv.innerHTML = 'Error fetching contributions.';
+//   });
+
 fetch('db.json')
   .then(response => response.json())
   .then(data => {
@@ -7,7 +50,18 @@ fetch('db.json')
     if (data.contributions.length === 0) {
       contributionsDiv.innerHTML = 'No contributions found.';
     } else {
-      data.contributions.forEach(contribution => {
+      const contributions = data.contributions;
+      const halfLength = Math.ceil(contributions.length / 2);
+
+      const leftColumn = document.createElement('div');
+      leftColumn.classList.add('column');
+      contributionsDiv.appendChild(leftColumn);
+
+      const rightColumn = document.createElement('div');
+      rightColumn.classList.add('column');
+      contributionsDiv.appendChild(rightColumn);
+
+      contributions.forEach((contribution, index) => {
         const contributionDiv = document.createElement('div');
         contributionDiv.classList.add('contribution');
 
@@ -31,7 +85,11 @@ fetch('db.json')
         message.textContent = contribution.message;
         contributionDiv.appendChild(message);
 
-        contributionsDiv.appendChild(contributionDiv);
+        if (index < halfLength) {
+          leftColumn.appendChild(contributionDiv);
+        } else {
+          rightColumn.appendChild(contributionDiv);
+        }
       });
     }
   })
